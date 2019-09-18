015234169748d7e8d176ab4a3727cffa2a79ee99
laura-daugherty <49730401+laura-daugherty@users.noreply.github.com>
2019-09-17 20:10:47 -0700

    Add files via upload

---
+++ db.js
@@ -0,0 +1,7 @@
+module.exports =  {
+  database: 'postgres',
+  host: 'serverless-practice.cbqfygasbzut.us-east-2.rds.amazonaws.com',
+  port: '5432',
+  user: 'master',
+  password: 'serverless',
+}
